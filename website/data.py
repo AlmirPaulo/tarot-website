e747e3a95e9a26b48fd8cb3e6501d81df220e720
AlmirPaulo <ap.freelas@gmail.com>
2021-09-10 22:19:33 -0300

    structure done!

---
+++ website/data.py
@@ -0,0 +1,65 @@
+FOOL = {"title": "The Fool", "meaning": []}
+
+MAGICIAN = {
+    "title": "The Magician",
+    "meaning": [
+        "What is he doing?",
+        "Why he is in this stance?",
+        "What means the sword on the table?",
+        "What means the staff  on the table?",
+        "What means the chalice on the table?",
+        "What means the coin on the table?",
+        "What about the flowers in front of him?",
+    ],
+}
+
+HIGH_PRIESTESS = {"title": "The High Priestess", "meaning": []}
+
+EMPRESS = {"title": "The Empress", "meaning": []}
+
+EMPEROR = {"title": "The Emperor", "meaning": []}
+
+
+HIEROPHANT = {"title": "The Hierophant", "meaning": []}
+
+
+LOVERS = {"title": "The Lovers", "meaning": []}
+
+
+CHARIOT = {"title": "The Chariot", "meaning": []}
+
+
+STRENGTH = {"title": "The Strength", "meaning": []}
+
+
+HERMIT = {"title": "The Hermit", "meaning": []}
+
+
+WHEEL = {"title": "The Wheel of Fortune", "meaning": []}
+
+
+JUSTICE = {"title": "The Justice", "meaning": []}
+
+
+HANGED_MAN = {"title": "The Hanged Man", "meaning": []}
+
+
+DEATH = {"title": "The Death", "meaning": []}
+
+TEMPERANCE = {"title": "The Temperance", "meaning": []}
+
+DEVIL = {"title": "The Devil", "meaning": []}
+
+TOWER = {"title": "The Tower", "meaning": []}
+
+STAR = {"title": "The Star", "meaning": []}
+
+MOON = {"title": "The Moon", "meaning": []}
+
+
+SUN = {"title": "The Sun", "meaning": []}
+
+JUDGEMENT = {"title": "The Judgement", "meaning": []}
+
+
+WORLD = {"title": "The World", "meaning": []}
--- website/main.py
+++ website/main.py
@@ -1,21 +1,63 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from data import *
+import random, requests
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/card')
-def card():
-    return render_template('1.html')
 
-#factory
+@app.route("/cards")
+def cards():
+    return render_template('cards.html')
+    
+
+@app.route("/yourcard")
+def your_card():
+    pick = random.randint(0, 21)
+    pic = f"imgs/cards/rws/{str(pick)}.jpg"
+    res = requests.get("http://localhost:5000/api")
+    data = res.json()
+
+    return render_template("card.html", pick=pick, pic=pic, data=data)
+
+
+@app.route("/api")
+def get_data():
+    json = [
+        FOOL,
+        MAGICIAN,
+        HIGH_PRIESTESS,
+        EMPRESS,
+        EMPEROR,
+        HIEROPHANT,
+        LOVERS,
+        CHARIOT,
+        STRENGTH,
+        HERMIT,
+        WHEEL,
+        JUSTICE,
+        HANGED_MAN,
+        DEATH,
+        TEMPERANCE,
+        DEVIL,
+        TOWER,
+        STAR,
+        MOON,
+        SUN,
+        JUDGEMENT,
+        WORLD,
+    ]
+    return jsonify(json)
+
+
+# factory
 def create_app():
     return app
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     create_app().run(debug=True)
