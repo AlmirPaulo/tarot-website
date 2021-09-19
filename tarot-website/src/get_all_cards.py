5a6ba9cf751a43b490301f121b1b9395771eee60
AlmirPaulo <ap.freelas@gmail.com>
2021-09-19 18:29:17 -0300

    done

--- tarot-website/src/get_all_cards.py
+++ tarot-website/src/get_all_cards.py
@@ -3,32 +3,29 @@ from browser import ajax, html, console
 from javascript import JSON 
 import random
 
-
-#variables
-card_img = doc['card']
-title = doc['title']
-questions = doc['questions']
+box = doc['box']
 
 #Ajax call
 def get_data(req):
     if req.status == 200 or req.status == 0:
+        box.text = ''
         res = JSON.parse(req.response)
-        pick = random.randint(0, 21)
-        console.log(pick, res)
-        title.innerText = res['cards'][pick]['title']
-        card_img.attrs['src'] = res['cards'][pick]['picture']
-        for q in res['cards'][pick]['questions']:
-           questions <= html.LI(q)  
+        cards = []
+        for n in range(0,22):
+           cards.append(res['cards'][n]['picture'])
+           
+        for i in cards:
+            box  <= html.IMG(src=i, id="card", Class="m-2")
+
     else:
         print('error code 500')
 
 def loading(req):
-    title <= html.H3('loading...')
+    box <= html.H3('loading...')
 
 req = ajax.Ajax()
 req.bind('complete', get_data)
 req.bind('loading', loading)
 req.open('GET', 'https://tarotapi.almirpaulo.repl.co/', False)
 req.send()
-
 
